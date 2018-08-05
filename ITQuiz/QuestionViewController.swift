0ec339b811e9b968d51ef0bea7ca07777d7e4028
Yudai Terashita <y.t.sougou@gmail.com>
2018-08-05 20:06:46 +0900

    松井さん修正

--- ITQuiz/QuestionViewController.swift
+++ ITQuiz/QuestionViewController.swift
@@ -9,50 +9,33 @@
 import UIKit
 import AudioToolbox
 
-class QuestionViewController:UIViewController {
+class QuestionViewController:UIViewController{
     
     
-     func csvToArray () {
-        if let csvPath = Bundle.main.path(forResource: "question", ofType: "csv") {
-            do {
-                let csvStr = try String(contentsOfFile:csvPath, encoding:String.Encoding.utf8)
-                let csvArr = csvStr.components(separatedBy: .newlines)
-                print(csvArr)
-            } catch let error as NSError {
-                print(error.localizedDescription)
-            }
-        }
-    }
-   
-    
-    
-    var questionData:QuestionData
-    
+    var questionData: QuestionData!
     
     @IBOutlet weak var questionNoLabel: UILabel!
-        //問題番号ラベル
+    //問題番号ラベル
     @IBOutlet weak var questionTextView: UITextView!
-        //問題文テキストビュー
-
+    //問題文テキストビュー
+    
     @IBOutlet weak var answer1Button: UIButton!
-        //選択肢1ボタン
+    //選択肢1ボタン
     @IBOutlet weak var answer2Button: UIButton!
-        //選択肢2ボタン
+    //選択肢2ボタン
     @IBOutlet weak var answer3Button: UIButton!
-        //選択肢3ボタン
+    //選択肢3ボタン
     @IBOutlet weak var answer4Button: UIButton!
-        //選択肢4ボタン
+    //選択肢4ボタン
     
     @IBOutlet weak var correctImageView: UIImageView!
-        //正解のイメージビュー
+    //正解のイメージビュー
     @IBOutlet weak var incorrectImageView: UIImageView!
-        //不正解のイメージビュー
+    //不正解のイメージビュー
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        csvToArray()
         
         let test = QuestionDataManager.sharedInstance
         
@@ -62,10 +45,6 @@ class QuestionViewController:UIViewController {
         guard let questionData = test.nextQuestion() else{
             return
         }
-        
-        
-        print(questionData.question)
-        
         //初期データ設定処理。前画面で設定済みのquestionDataから値を取り出す
         questionNoLabel.text = "Q.\(questionData.questionNo)"
         questionTextView.text = questionData.question
@@ -73,12 +52,13 @@ class QuestionViewController:UIViewController {
         answer2Button.setTitle(questionData.answer2,for:UIControlState.normal)
         answer3Button.setTitle(questionData.answer3,for:UIControlState.normal)
         answer4Button.setTitle(questionData.answer4,for:UIControlState.normal)
+        
+        self.questionData = questionData
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
     
     //選択肢1をタップ
     @IBAction func tapAnswer1Button(_ sender:Any){
@@ -106,10 +86,10 @@ class QuestionViewController:UIViewController {
         //正解しているか判定する
         if questionData.isCorrect(){
             //正解のアニメーションをしながら次の問題へ遷移する
-            goNextQuestionWithAnimation()
-        }else{
+            goNextQuestionWithCorrectAnimation()
+        } else {
             //不正解のアニメーションをしながら次の問題へ遷移する
-            goNextQuestionWithAnimation()
+            goNextQuestionWithInCorrectAnimation()
         }
     }
     
@@ -121,10 +101,10 @@ class QuestionViewController:UIViewController {
         UIView.animate(withDuration: 2.0, animations: {
             //アルファ値を1.0に変化される(初期値はstoryboardで0.0に設定済み)
             self.correctImageView.alpha = 1.0
-            }){(Bool) in self.goNextQuestion()    //アニメーション完了後に次の問題に進む
+        }){(Bool) in self.goNextQuestion()    //アニメーション完了後に次の問題に進む
         }
     }
-
+    
     //次の問題に不正解のアニメーション付きで遷移する
     func goNextQuestionWithInCorrectAnimation(){
         //不正解を伝える音を鳴らす
@@ -133,16 +113,20 @@ class QuestionViewController:UIViewController {
         UIView.animate(withDuration: 2.0, animations: {
             //アルファ値を1.0に変化される(初期値はstoryboardで0.0に設定済み)
             self.incorrectImageView.alpha = 1.0
-            }){(Bool) in self.goNextQuestion()    //アニメーション完了後に次の問題に進む
+        }){(Bool) in self.goNextQuestion()    //アニメーション完了後に次の問題に進む
         }
     }
-
+    
     //次の問題へ遷移する
     func goNextQuestion(){
         //問題文がある場合は次の問題へ遷移する
         guard let nextQuestion = QuestionDataManager.sharedInstance.nextQuestion() else{
             //問題文がなければ結果画面へ遷移する
             //storyboradのIdentifierに設定した値(result)を指定してViewControllerを生成する
+            
+            /*
+             * ここで次のストーリーボードがないから落ちる(2018/07/28 松井)
+             */
             if let resultViewController = storyboard?.instantiateViewController(withIdentifier: "result") as? ResultViewController{
                 //storyboardのsegueを利用しない明示的な画面遷移処理
                 present(resultViewController,animated:true,completion:nil)
@@ -151,12 +135,17 @@ class QuestionViewController:UIViewController {
         }
         // 問題文がある場合は次の問題へ遷移する
         // StoryboardのIdentifierに設定した値(question)を設定してViewControllerを生成する
+        
+        
+        /*
+         * ここで次のストーリーボードがないから落ちる(2018/07/28 松井)
+         */
         if let nextQuestionViewController = storyboard?.instantiateViewController(withIdentifier: "question") as? QuestionViewController{
             nextQuestionViewController.questionData = nextQuestion
             // StoryboardのSegueを利用しない明示的な画面遷移処理
             present(nextQuestionViewController, animated: true,completion: nil)
-        
-        
+            
+            
         }
     }
 }
